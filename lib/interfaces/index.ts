c662631fd836c13b4560cd265f9394a024273249
Emir Muhammadzadeh <AMIR.MSC90@GMAIL.COM>
2024-02-27 21:52:07 +0330

    Async initializer added (#8)
    * v0.0.16
    
    * forRootAsync method added

--- lib/interfaces/index.ts
+++ lib/interfaces/index.ts
@@ -1,2 +1,1 @@
 export * from './storage-module-options';
-export * from './storage-options-factory';
--- lib/interfaces/storage-module-options.ts
+++ lib/interfaces/storage-module-options.ts
@@ -1,7 +1,15 @@
+import { FactoryProvider, ModuleMetadata } from '@nestjs/common';
 import { StorageOption } from '../types';
 
 export interface StorageModuleOptions {
   provider: 's3' | 'local' | 'gcs' | 'azure' | 'r2';
   options: StorageOption;
+}
+
+export interface AsyncStorageModuleOptions extends Pick<ModuleMetadata, 'imports'> {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<StorageModuleOptions> | StorageModuleOptions;
+  inject?: FactoryProvider['inject'];
 }
 
